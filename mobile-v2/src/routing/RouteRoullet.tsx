c7ed0db30303e01297b35444836ea7cdffb757d8
Александр Утбанов <utbanovag@it2g.ru>
2020-08-26 01:45:21 +0500

    add route roullet

---
+++ mobile-v2/src/routing/RouteRoullet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+export const RouteRoullet = () => {
+    return (
+        <Router>
+            <Switch>
+                
+            </Switch>
+        </Router>
+    )
+}
---
+++ mobile-v2/src/routing/index.ts
@@ -0,0 +1,1 @@
+export { RouteRoullet } from "./RouteRoullet";
