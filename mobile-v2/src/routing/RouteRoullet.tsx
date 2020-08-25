5413f3ca0e835b1e8da310db622385a9789de7fa
Александр Утбанов <utbanovag@it2g.ru>
2020-08-26 02:04:42 +0500

    add ios

--- mobile-v2/src/routing/RouteRoullet.tsx
+++ mobile-v2/src/routing/RouteRoullet.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Button } from "antd";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 export const RouteRoullet = () => {
     return (
         <Router>
             <Switch>
-                
+                <Route path="/" exact>
+                    <>
+                        <h1>Hello custom capacitor</h1>
+                        <Button size="large">CLICK</Button>
+                    </>
+                </Route>
+                <Route path="/just" exact>
+                    <h1>Hello just capacitor</h1>
+                </Route>
             </Switch>
         </Router>
     )
