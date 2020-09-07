cdf2757d75e380a7cad3a27f8be977305a24733e
Alexander Utbanov <utbanovag@it2g.ru>
2020-09-07 18:54:00 +0500

    update authorization

--- mobile-v2/src/pages/Authorization/Authorization.tsx
+++ mobile-v2/src/pages/Authorization/Authorization.tsx
@@ -1,13 +1,26 @@
-import React, { FC } from "react";
+import React, { FC, useState, useMemo } from "react";
 
 import "./style.scss";
 import { LoginForm, RegistrationForm } from "./components";
 
+enum AuthFormNames {
+  login = "login",
+  registration = "registration"
+}
+
 export const Authorization: FC = (props) => {
+  const [activeForm, setActiveForm] = useState(AuthFormNames.login);
+  const isLogin = activeForm === AuthFormNames.login;
+
+  const authForm = useMemo(() => isLogin ? <LoginForm /> : <RegistrationForm />, [isLogin]);
+  const formChanger = useMemo(() => isLogin ? <>У вас есть аккаунт? <a>Вход</a></> : <a>Регистрация</a>, [isLogin]);
+
   return (
     <div className="authorization">
-      <LoginForm />
-      {/* <RegistrationForm /> */}
+      {authForm}
+      <div className="authorization-form-changer">
+        {formChanger}
+      </div>
     </div>
   )
 }
