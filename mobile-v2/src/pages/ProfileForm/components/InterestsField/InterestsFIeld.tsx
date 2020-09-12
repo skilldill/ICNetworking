858e8deb427e69003b3d6a5608063a083ac00b3d
Александр Утбанов <utbanovag@it2g.ru>
2020-09-13 01:35:53 +0500

    update interests field

--- mobile-v2/src/pages/ProfileForm/components/InterestsField/InterestsFIeld.tsx
+++ mobile-v2/src/pages/ProfileForm/components/InterestsField/InterestsFIeld.tsx
@@ -1,13 +1,51 @@
-import React, { FC } from "react";
+import React, { FC, useState, useCallback } from "react";
 
 import "./style.scss";
 import { Input } from "shared/components";
 
+const INTERESTS_MOCK = [
+    { name: "Футбол" },
+    { name: "Фудблогинг" },
+    { name: "Спорт" },
+    { name: "Сабли" },
+    { name: "Сальса" },
+    { name: "Фехтование" }
+]
+
 export const InterestsField: FC = (props) => {
+    const [currentValue, setCurrentValue] = useState("");
+    const [foundIterests, setFoundInterests] = useState<{name: string}[]>([]);
+    const [selectedInterests, setSelectedInterests] = useState([]);
+
+    const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.currentTarget;
+        setCurrentValue(value);
+
+        // TEST
+        const prepareFoundInterests = INTERESTS_MOCK.filter((interest) => interest.name.includes(value));
+        setFoundInterests(prepareFoundInterests);
+    }, [currentValue])
+
     return (
         <div className="interests-field">
             <h3>Мои интересы</h3>
-            <Input placeholder="Введите интересы" clear />
+            {!!selectedInterests.length ? (
+                <div></div>
+            ) : (
+                <>
+                    <Input 
+                        placeholder="Введите интересы" 
+                        value={currentValue}
+                        onChange={handleChange}
+                        onClear={() => { setCurrentValue("") }}
+                    />
+                    {!!foundIterests.length && (
+                        <ul className="found-interests">
+
+                        </ul>
+                    )}
+                </>
+            )}
         </div>
     )
 }
--- mobile-v2/src/shared/components/Input/Input.tsx
+++ mobile-v2/src/shared/components/Input/Input.tsx
@@ -7,17 +7,22 @@ import { CrossInputSVG } from "assets/icons";
 
 interface CustomInputProps extends InputProps {
   label?: string,
-  clear?: boolean
+  onClear?: () => void
 }
 
 export const Input: FC<CustomInputProps> = (props) => {
-  const { label, clear } = props;
+  const { label, onClear } = props;
+
+  const handleClear = () => {
+    !!onClear && onClear();
+  }
+
   return (
     <div className="control-input">
       {!!label && <small>{label}</small>}
       <AntInput {...props} />
-      {clear && (
-        <button className="clear-btn">
+      {!!onClear && (
+        <button className="clear-btn" onClick={handleClear}>
           <img src={CrossInputSVG} alt="clear"/>
         </button>
       )}
