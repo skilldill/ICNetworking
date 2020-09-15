f880f06abee13f19d2cf236f2d8cec1f71631be0
Alexander Utbanov <utbanovag@it2g.ru>
2020-09-15 09:10:12 +0500

    update FadePage add events

--- mobile-v2/src/core/FadePage/FadePage.tsx
+++ mobile-v2/src/core/FadePage/FadePage.tsx
@@ -1,13 +1,33 @@
-import React, { FC, useMemo, CSSProperties, useCallback } from "react";
+import React, { FC, useMemo, CSSProperties, useCallback, useEffect } from "react";
 import "./style.scss";
 
 interface FadePageProps {
   show: boolean,
   direction: "vertical" | "horizontal"
+  onOpen?: () => void;
+  onClose?: () => void;
 }
 
 export const FadePage: FC<FadePageProps> = (props) => {
-  const { children, show, direction } = props;
+  const { children, show, direction, onOpen, onClose } = props;
+
+  useEffect(() => {
+
+    // DURATION NEEDS FOR ANIMATION END
+    if (!!onOpen && show) {
+      const timeout = setTimeout(() => {
+        onOpen();
+        clearTimeout(timeout);
+      }, 300);
+    } 
+
+    if (!!onClose && !show) {
+      const timeout = setTimeout(() => {
+        onClose();
+        clearTimeout(timeout);
+      }, 300);
+    }
+  }, [show])
 
   const fadeStyle = useMemo((): CSSProperties => direction === "horizontal" ? ({ 
     transform: `translateX(${show ? 0 : window.innerWidth}px)` }) : ({
