5bc67b55e6d4f07cdcac2a68e8b308627824f702
Александр Утбанов <utbanovag@it2g.ru>
2020-10-24 23:48:06 +0500

    add test get matches

--- mobile-v2/src/pages/Matches/Matches.tsx
+++ mobile-v2/src/pages/Matches/Matches.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.scss";
 import { Navbar } from "core/Navbar";
+import { http } from "shared/http";
 
 export const Matches = () => {
+
+    // test matches
+    useEffect(() => {
+        const getMatches = async () => {
+            try {
+                await http.get('/api/events/matches/');
+            } catch(error) {
+                console.log(error.message);
+            }
+        }
+
+        getMatches();
+    },[])
+
     return (
         <div className="matches">
             <Navbar title="Совпадения" />
