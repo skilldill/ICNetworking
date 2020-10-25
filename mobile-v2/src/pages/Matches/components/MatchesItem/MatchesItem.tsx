aa9f7407f062b613015e48b51bac7ae21571fa65
Александр Утбанов <utbanovag@it2g.ru>
2020-10-25 20:58:41 +0500

    add render matches list

--- mobile-v2/src/pages/Matches/components/MatchesItem/MatchesItem.tsx
+++ mobile-v2/src/pages/Matches/components/MatchesItem/MatchesItem.tsx
@@ -1,8 +1,9 @@
 import Item from "antd/lib/list/Item";
-import React, {FC} from "react";
+import React, {FC, useMemo} from "react";
 import { PartBlock } from "shared/components";
 
 import "./style.scss";
+import { ProfileAvatarSVG } from "assets/icons";
 
 interface MacthesItemProps {
     match: any,
@@ -11,10 +12,28 @@ interface MacthesItemProps {
 
 export const MacthesItem: FC<MacthesItemProps> = (props) => {
     const { match, onLongPress } = props;
+    const { firstName, lastName, positionName, avatars } = match;
+
+    const fullName = useMemo(() => `${firstName} ${lastName}`, [firstName, lastName]);
+    const position = useMemo(() => !!positionName ? positionName : 'Не указано', [positionName]);
+
+    const avatar = useMemo(() => (!!avatars && avatars.length) ? (
+        <div className="avatar">
+            <img src={avatars[avatars.length - 1].picture} alt={firstName} />
+        </div>
+    ) : (
+        <div className="avatar avatar-mock">
+            <img src={ProfileAvatarSVG} alt={fullName} />
+        </div>
+    ), [avatars, fullName])
 
     return (
         <PartBlock className="matches-item">
-            
+            {avatar}
+            <div>
+                <p className="fullname">{fullName}</p>
+                <p className="position">{position}</p>
+            </div>
         </PartBlock>
     )
 }
