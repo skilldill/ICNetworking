644317c380ec8fdcc81abf85b137d5a250dee459
Утбанов Александр <utbanovag@it2g.ru>
2020-10-30 00:04:13 +0500

    add selct members

--- mobile-v2/src/pages/Matches/components/MatchesAdd/MatchesAdd.tsx
+++ mobile-v2/src/pages/Matches/components/MatchesAdd/MatchesAdd.tsx
@@ -1,26 +1,45 @@
-import React, { FC, useCallback } from "react";
+import React, { FC, useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import "./style.scss";
-import { PlusWhiteSVG } from "assets/icons";
+import { PlusWhiteSVG, ProfileAvatarSVG } from "assets/icons";
 import { matchesModule } from "store/matches";
 
 export const MatchesAdd: FC = (props) => {
-    const { selectMode } = useSelector(matchesModule.selector);
+    const { selectMode, selectedIds, matches } = useSelector(matchesModule.selector);
     const dispatch = useDispatch();
 
     const handleChangeSelectMode = useCallback(() => {
         dispatch(matchesModule.actions.setSelectMode(!selectMode));
     }, [selectMode])
+    
+    const selectedCollegues = useMemo(() => matches.filter((match) => selectedIds.includes(match.id)), [selectedIds, matches]);
+    const members = useMemo(() => !!selectedCollegues && (
+        <div className="members">
+            {selectedCollegues.map((member, i) => 
+                <div className="member" key={i}>
+                    <div className="circle" onClick={handleChangeSelectMode}>
+                        {!!member.avatars.length ? (
+                            <img className="avatar" src={member.avatars[member.avatars.length - 1].picture} alt={member.name} />
+                        ): (
+                            <img className="avatar-mock" src={ProfileAvatarSVG} alt={member.name} />
+                        )}
+                    </div>
+                    <p>{member.firstName}</p>
+                </div>
+            )}
+        </div>
+    ), [selectedCollegues])
 
     return (
         <div className="matches-add">
             <div className="add-members">
                 <div className="circle-add" onClick={handleChangeSelectMode}>
-                    <img src={PlusWhiteSVG} alt="Добавить"/>
+                    <img src={PlusWhiteSVG} alt="Добавить" />
                 </div>
                 <p>Пригласить</p>
             </div>
+            {members}
         </div>
     )
 }
